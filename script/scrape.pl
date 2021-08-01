b5cb66e0858d519d379e00804b277b1e35740c77
Max Maischein <corion@corion.net>
2021-08-01 20:47:40 +0200

    Add an updater, some example URLs and a scraper for our store

---
+++ script/scrape.pl
@@ -0,0 +1,199 @@
+#!perl -w
+use 5.012; # //=
+use strict;
+use warnings;
+use HTTP::Crawl::Store;
+use Getopt::Long;
+use Pod::Usage;
+use List::MoreUtils 'zip';
+use App::scrape 'scrape';
+
+=head1 NAME
+
+scrape.pl - simple HTML scraping from the command line
+
+=head1 ABSTRACT
+
+This is a simple program to extract data from HTML by
+specifying CSS3 or XPath selectors.
+
+=head1 SYNOPSIS
+
+    scrape.pl URL selector selector ...
+
+    # Print page title
+    scrape.pl http://perl.org title
+    # The Perl Programming Language - www.perl.org
+
+    # Print links with titles, make links absolute
+    scrape.pl http://perl.org a //a/@href --uri=2
+
+    # Print all links to JPG images, make links absolute
+    scrape.pl http://perl.org a[@href=$"jpg"]
+
+    # print JSON about Amazon prices
+    scrape.pl https://www.amazon.de/dp/0321751043
+        --format json
+        --name "title" #productTitle
+        --name "price" #priceblock_ourprice
+        --name "deal" #priceblock_dealprice
+
+    # print JSON about Amazon prices for multiple products
+    scrape.pl --format json
+        --url https://www.amazon.de/dp/B01J90P010
+        --url https://www.amazon.de/dp/B01M3015CT
+        --name "title" #productTitle
+        --name "price" #priceblock_ourprice
+        --name "deal" #priceblock_dealprice
+
+    # extract values from HTML from stdin
+    scrape.pl --format json
+        --url -
+        --name "title" #productTitle
+        --name "price" #priceblock_ourprice
+        --name "deal" #priceblock_dealprice
+
+=cut
+
+GetOptions(
+    'help|h'      => \my $help,
+    'uri=s'       => \my @make_uri,
+    'no-uri'      => \my $no_known_uri,
+    'sep=s'       => \my $sep,
+    'format=s'    => \my $format,
+    'name=s'      => \my @column_names,
+    'url=s'       => \my @urls,
+    'keep-url=s'  => \my $url_field,
+) or pod2usage(2);
+pod2usage(1) if $help;
+
+binmode *STDOUT, ':utf8';
+$format ||= 'csv';
+
+# make_uri can be a comma-separated list of columns to map
+# The index starts at one
+my %make_uri = map{ $_-1 => 1 } map{ split /,/ } @make_uri;
+$sep ||= "\t";
+
+# Now determine where we get the HTML to scrape from:
+if( ! @urls ) {
+    @urls = shift @ARGV;
+};
+
+my $args;
+if( @ARGV ) {
+    # we need columns here
+    @column_names == @ARGV
+        or die "Different number of column names and column expressions";
+
+    $args = +{ zip @column_names, @ARGV };
+
+} else {
+    $args = \@ARGV;
+}
+
+my @rows;
+
+my $store = HTTP::Crawl::Store->new(
+    dsn => 'dbi:SQLite:dbname=db/crawler.sqlite',
+);
+$store->connect();
+
+for my $url ( @urls ) {
+    # We assume that the URLs are in the store already, instead of
+    # optionally (re)fetching them if they are "too old"
+    say "Unstoring '$url'";
+    my $res = $store->retrieve_url( GET => $url );
+    my $html = $res->{content};
+
+    #my $title = ($res->{content} =~ m!<title>(.*?)</title>!i);
+
+    # now fetch all "rows" from the page. We do this once to avoid
+    # fetching a page multiple times
+    push @rows, scrape($html, $args, {
+        make_uri     => \%make_uri,
+        no_known_uri => $no_known_uri,
+        base         => $url,
+        url_field    => $url_field,
+    });
+};
+
+if( 'json' eq $format ) {
+    require JSON;
+    print JSON::encode_json(\@rows);
+
+} else {
+    require Text::CSV_XS;
+    Text::CSV_XS->import('csv');
+    csv( in => \@rows, out => \*STDOUT, sep_char => $sep );
+};
+
+=head1 DESCRIPTION
+
+This program fetches an HTML page and extracts nodes
+matched by XPath or CSS selectors from it.
+
+If URL is C<->, input will be read from STDIN.
+
+=head1 OPTIONS
+
+=over 4
+
+=item B<--format>
+
+Output format, the default is C<csv>. Valid values are C<csv> or C<json>.
+
+=item B<--url>
+
+URL to fetch. This can be given multiple times to fetch multiple URLs in
+one run. If this is not given, the first argument on the command line will be
+taken as the only URL to be fetched.
+
+=item B<--keep-url>
+
+Add the fetched URL as another column with the given name in the output.
+If you use CSV output, the URL will always be in the first column.
+
+=item B<--name>
+
+Name of the output column.
+
+=item B<--sep>
+
+Separator character to use for columns. Default is tab.
+
+=item B<--uri> COLUMNS
+
+Numbers of columns to convert into absolute URIs, if the
+known attributes do not everything you want.
+
+=item B<--no-uri>
+
+Switches off the automatic translation to absolute
+URIs for known attributes like C<href> and C<src>.
+
+=back
+
+=head1 REPOSITORY
+
+The public repository of this module is
+L<http://github.com/Corion/App-scrape>.
+
+=head1 SUPPORT
+
+The public support forum of this program is
+L<http://perlmonks.org/>.
+
+=head1 AUTHOR
+
+Max Maischein C<corion@cpan.org>
+
+=head1 COPYRIGHT (c)
+
+Copyright 2011-2021 by Max Maischein C<corion@cpan.org>.
+
+=head1 LICENSE
+
+This module is released under the same terms as Perl itself.
+
+=cut
---
+++ script/update.pl
@@ -0,0 +1,60 @@
+#!perl -w
+use 5.012; # //=
+use strict;
+use warnings;
+use HTTP::Crawl::Store;
+use Minion;
+use Minion::Backend::SQLite;
+use Mojo::UserAgent;
+use YAML::Tiny 'LoadFile';
+use Getopt::Long;
+use Pod::Usage;
+use POSIX 'strftime';
+use Time::Local 'timegm';
+
+# (Re)submits all URLs from a watchlist
+# The watchlist should move into SQLite instead of being YAML
+# but while I work on the schema, it being YAML is good enough
+
+GetOptions(
+    'config|c=s' =>  \my $config_file,
+    'force|f' =>  \my $force_update,
+) or pod2usage(2);
+
+$config_file //= 'urllist.yml';
+
+my $urls = LoadFile($config_file);
+
+my $minion = Minion->new( SQLite => 'sqlite:db/crawler.sqlite' );
+my $store = HTTP::Crawl::Store->new(
+    dsn => 'dbi:SQLite:dbname=db/crawler.sqlite',
+);
+$store->connect();
+
+my $ts = time();
+for my $task (@$urls) {
+    my $t = $task;
+    my $last_fetched = $store->retrieve_url( GET => $t->{url} );
+
+    my $fetch = 1;
+    if( $last_fetched ) {
+
+        my $next_fetch = $ts - $t->{update};
+        my $next_fetch_ts = strftime '%Y-%m-%d %H:%M:%SZ', gmtime( $next_fetch );
+
+        $fetch = $force_update || $last_fetched->{retrieved} lt $next_fetch_ts;
+
+        if( $fetch ) {
+            say "Updating $t->{url} ( $next_fetch_ts )";
+        } else {
+            say "$t->{url} is new enough, updating after $next_fetch_ts";
+        }
+
+    } else {
+        say "New URL $t->{url}";
+    }
+
+    if( $fetch ) {
+        $minion->enqueue(fetch_url => [ GET => $t->{url} ]);
+    };
+};
